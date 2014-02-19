98ceac05ce8de00bf205ee6efa0f5b4d05d5280b
Stefano Kowalke <Konafets@users.noreply.github.com>
2014-02-19 22:28:00 +0100

    Add support for Scrutinizer

--- executeUnitTests.sh
+++ executeUnitTests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if ./typo3conf/ext/phpunit/Composer/vendor/bin/phpunit -c typo3/sysext/core/Build/UnitTests.xml
 then
+  wget https://scrutinizer-ci.com/ocular.phar
+  php ocular.phar code-coverage:upload --format=php-clover coverage.clover
 	exit 0
 else
 	exit 99
