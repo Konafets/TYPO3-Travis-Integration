8abf9f39e59244abdf8cf067ce3c0bbc3de6e28d
Stefano Kowalke <Konafets@users.noreply.github.com>
2014-02-20 02:56:27 +0100

    Update executeUnitTests.sh

--- executeUnitTests.sh
+++ executeUnitTests.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if ./typo3conf/ext/phpunit/Composer/vendor/bin/phpunit --coverage-clover coverage.clover -c typo3/sysext/core/Build/UnitTests.xml
-then
-	exit 0
-else
-	exit 99
-fi
+./typo3conf/ext/phpunit/Composer/vendor/bin/phpunit --coverage-clover coverage.clover -c typo3/sysext/core/Build/UnitTests.xml
